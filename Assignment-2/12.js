fc5313e2216aade6856f0b6a94e7dfc33143f180
asu1609 <asupadhyay1609@gmail.com>
2023-02-15 14:58:09 +0530

    edited files

---
+++ Assignment-2/12.js
@@ -0,0 +1,3 @@
+let quote = '"The unexamined life is not worth living." - Socrates';
+let arr = quote.split('-');
+console.log(arr);
---
+++ Assignment-2/13.js
@@ -0,0 +1,3 @@
+let email = "asupadhyay1609@gmail.com";
+let exp = /[a-z0-9]+@[a-z0-9]+\.[a-z]{2,4}/;
+console.log(exp.test(email));
